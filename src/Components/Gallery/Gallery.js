0b34ae00324a2ba8dc2f3c6d0584e1b37a57efd1
basuabhirup <basu.abhirup@yahoo.com>
2020-08-26 22:13:57 +0530

    updated validatin

--- src/Components/Gallery/Gallery.js
+++ src/Components/Gallery/Gallery.js
@@ -13,25 +13,28 @@ class Gallery extends React.Component{
             currentAddress: ''
         }
     }
-
+    //following will be executed on changing the input text field:
     onAddressChange = (event) => {
         this.setState({
             currentAddress: event.target.value
         })
 
     }
-
+    //following will be executed on clicking the "Add" button:
     onAddImage = () => {
+        if (this.state.currentAddress==="") {
+            return window.alert("Please give an image URL")
+        }
         //Copy all previous images
         const newImages = [...this.state.images]
 
-        //Add new image addess
+        //Add new image address
         newImages.push(this.state.currentAddress)
 
         //update state by setstate
         this.setState({
             images: newImages,
-            currentAddress: '',
+            currentAddress: ''
         })
     }
 
@@ -40,23 +43,29 @@ class Gallery extends React.Component{
     render = () =>{
         return(
             <div className="root-container">
+
                 <p className="title">
                     ----IMAGE GALLERY----</p>
+
                 <div className="input-box">
-                    <input type="text" 
+    {/* onAdressChange function will be called now: */}
+                    <input type="text"
+                        value = {this.state.currentAddress}
                         onChange={this.onAddressChange}
                         placeholder="Enter image URL"/>
+    {/* onAddImage function will be called now: */}
                     <button onClick={this.onAddImage}>Add</button>
                 </div>
 
                 <div className="list-box">
                 {this.state.images.map((url) => {
                     return (
-                    <ImageCard key={`${url}-${Math.random()*10}`} 
-                            imageURL={url}></ImageCard>
+                        <ImageCard key={url} 
+                                    imageURL={url}></ImageCard>
                     )
                 })}
                 </div>
+
             </div>
         );
     };
